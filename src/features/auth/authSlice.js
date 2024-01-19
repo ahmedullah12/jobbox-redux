6e4e7e1c27c3a920ce47b2254933b380ff019c49
ahmedullah12 <gulamjoy321@gmail.com>
2024-01-19 20:14:44 +0600

    firebase sign up added

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -1,17 +1,42 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import auth from "../../firebase/firebase.config";
 
 const initialState = {
     email: '',
     role: '',
-    isloading: true,
+    isLoading: true,
     isError: false,
     error: '',
 };
 
+export const createUser = createAsyncThunk("auth/createUser", async({email, password}) => {
+    const data = await createUserWithEmailAndPassword(auth, email, password);
+    return data.user.email;
+})
+
 const authSlice = createSlice({
     name: "auth",
     initialState,
-    
+    extraReducers: (builder) => {
+        builder.addCase(createUser.pending, (state) => {
+            state.isLoading =  true;
+            state.isError = false;
+            state.error = '';
+        })
+        .addCase(createUser.fulfilled, (state, action) => {
+            state.isLoading =  false;
+            state.email = action.payload;
+            state.isError = false;
+            state.error = '';
+        })
+        .addCase(createUser.rejected, (state, action) => {
+            state.isLoading =  false;
+            state.email = '';
+            state.isError = true;
+            state.error = action.error.message;
+        })
+    }
 });
 
 
